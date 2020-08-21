23de5f127991d3188193ab0efafd02be463a21f6
wsa <dannvx@gmail.com>
2020-08-21 16:34:47 +0300

    migrate formGroup to using _Group and doFormGroup to using GroupHalf -- the code is a rough sketch, doesnt work

--- src/sophos-grid.js
+++ src/sophos-grid.js
@@ -34,15 +34,16 @@ function formGroups(space, s, items) {
   return groupsByItem.toArray()
 }
 
-/*
+/**
+  @param {Int} originI index of the origin item in the sequence, from which
+  items are taken
 */
-
 class _Group {
-  constructor(origin, spaceToFill, before, after) {
-    this.origin = origin
+  constructor(origin, originI, spaceToFill, before, after) {
+    this.origin = origin; this.originI = originI
+    this.spaceToFill = spaceToFill
     this.before = before || null
     this.after = after || null
-    this.spaceToFill = spaceToFill
   }
 
   get reachedLeftLimit() {
@@ -51,14 +52,22 @@ class _Group {
       : false
   }
 
+  set reachedLeftLimit() {
+    throw new Error('reachedLeftLimit is read-only')
+  }
+
   get reachedRightLimit() {
     return (this.after)
       ? this.after.reached : false
   }
 
+  set reachedRightLimit() {
+    throw new Error('reachedRightLimit is read-only')
+  }
+
   get spaceLeft() {
     return this.spaceToFill - (
-      1 +
+      this.origin.space +
       (this.before && 'number' === typeof(this.before.spaceLeft) || 0) +
       (this.after && 'number' === typeof(this.after.spaceLeft) || 0)
     )
@@ -85,6 +94,43 @@ class _Group {
   set originI() {
     throw new Error('originI is read-only')
   }
+}
+
+function formGroup(spaceToFill, i, s) {
+  let spaceLeft = spaceToFill
+  spaceLeft -= s[i].space
+
+  if (space <= 0) {
+    return new _Group(s[i], i, spaceToFill, null, null)
+    // return {
+    //   g: [i], spaceLeft: space,
+    //   reachedLeftLimit: true, reachedRightLimit: true,
+    // }
+  }
+
+  const after = doFormGroup(1, space, i, s, [])
+  console.log('formGroup, after', after)
+
+  if (after.spaceLeft === 0) {
+    return new _Group(s[i], i, spaceToFill, null, after)
+    // return {
+    //   g: [i].concat(after.g),
+    //   spaceLeft: after.spaceLeft,
+    //   reachedLeftLimit: false,
+    //   reachedRightLimit: !!after.reached
+    // }
+  }
+
+  const before = doFormGroup(-1, after.spaceLeft, i, s, [])
+  console.log('formGroup, before', before)
+
+  return new _Group(s[i], i, spaceToFill, before, after)
+  // return {
+  //   g: before.g.concat([i].concat(after.g)),
+  //   spaceLeft: before.spaceLeft,
+  //   reachedRightLimit: after.reached,
+  //   reachedLeftLimit: before.reached
+  // }
 }
 
 function formGroup(space, i, s) {
@@ -130,11 +176,19 @@ class GroupHalf {
 }
 */
 
+class GroupHalf {
+  constructor(sequence, spaceLeft, reachedLimit) {
+    this.sequence = sequence
+    this.spaceLeft = spaceLeft
+    this.reachedLimit = reachedLimit
+  }
+}
+
 function doFormGroup(d, spaceLeft, i, s, g) {
   if (i+d > s.length-1 || i+d < 0) {
     // $Group.Reaching-Limit.3
-    // console.log('doFormGroup, i+d > s.length-1 || i+d < 0');
-    return {reached: true, spaceLeft, g}
+    return new GroupHalf(g, spaceLeft, true)
+    // return {reached: true, spaceLeft, g}
   }
 
   console.log('doFormGroup, d, spaceLeft, i', d, spaceLeft, i)
@@ -149,10 +203,13 @@ function doFormGroup(d, spaceLeft, i, s, g) {
     return doFormGroup(d, spaceLeftNew, i, s, g)
   } else if (spaceLeftNew === 0) {
     (d > 0) ? g.push(i+d) : g.unshift(i+d)
-    return {g, spaceLeft: spaceLeftNew}
+
+    return new GroupHalf(g, spaceLeftNew, false)
+    // return {g, spaceLeft: spaceLeftNew}
   } else {
     // $Group.Reaching-Limit.2
-    return {reached: true, spaceLeft, g}
+    return new GroupHalf(g, spaceLeft, true)
+    // return {reached: true, spaceLeft, g}
   }
 }
 
