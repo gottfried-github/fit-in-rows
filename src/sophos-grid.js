2d27c58ebdd437c2479791207c45aa861af0590b
osmomysl <dannvx@gmail.com>
2023-02-24 23:54:58 +0200

    cleanup

--- src/sophos-grid.js
+++ src/sophos-grid.js
@@ -1,57 +1,4 @@
-// const {randomBinaryProportionateSeqSecond} = require('./test-helpers')
-// const {permutationsToCombinations, getSize, isRatioEqual, sortItemsByType} = require('./schema-permutations')
 import {size, delta, isDeltaEmpty, overlaps, containsSubsequences} from './helpers.js'
-
-/**
-function main(groupSchemas) {
-
-}
-
-// itemTypes: [Int]
-// groupSize: {min: Int, max: Int}
-function formAllSequences(itemTypes, groupSize) {
-  const groupSizes = []
-  let size = groupSize.min
-  while (size < groupSize.max) {
-    groupSizes.push(size++)
-  }
-}
-*/
-
-/**
- * @param {[Array, Array, ...]} subSs `subsequence`s where each `subsequence`'s first `item` is subsequent to the previous one's in a `sequence`
- * @comment This has been only tried on homogeneous subsequences
-*/
-function negateOverlaps(subSs) {
-  return doNegateOverlaps(subSs, [], doNegateOverlaps)
-}
-
-function doNegateOverlaps(subSs, sequences, negate) {
-  if (0 === subSs.length) return sequences
-
-  return negate(subSs,
-    cascadeSubsequence(subSs.shift(), sequences, cascadeSubsequence), negate
-  )
-}
-
-/**
- * @param {Array} subS a `subsequence`
- * @param {[Array, Array, ...] || []} sequences of `subsequence`s
- * @returns sequences of `subsequence`s, represented by `ref`s, where none of the `subsequence`s overlap
- * @description appends the subS to the first sequence in the sequences where it doesn't overlap with the last `subsequence` or appends a new sequence, containing the subS, to the sequences if all sequences overlap
-*/
-function cascadeSubsequence(subS, sequences, cascade) {
-  if (0 === sequences.length) return [[subS]]
-
-  if (!overlaps(sequences[0][sequences[0].length-1], subS)) {
-    sequences[0].push(subS)
-    return sequences
-  }
-
-  // process or create next sequence
-  const s = sequences.shift()
-  return [s, ...cascade(subS, sequences, cascade)]
-}
 
 /**
  * @param {Array} sequence a `sequence`
@@ -99,7 +46,7 @@ function subsequencesSequences(sequence, subsequences, subsequencesAll) {
 
     // skip recursion if space between the sequence and the subsequence can contain subsequences
     if (containsSubsequences(subsequenceLast ? subsequenceLast[subsequenceLast.length-1] : -1, subsequence[0], subsequencesAll.reduce((_subsequences, subsequences) => {_subsequences.push(...subsequences); return _subsequences}, []))) return
-    
+
     sequences.push(...subsequencesSequences(_sequence, _subsequences, subsequences))
   })
 
@@ -154,16 +101,6 @@ function fill(space, sequence) {
     : fillSpace(space, [...sequence], [], fillSpace)
 }
 
-/*
-function formSubsequence(space, sequence) {
-  return new Subsequence(
-    space, Array.isArray(space)
-      ? fillSchema([...space], [...sequence], [], fillSchema)
-      : fillSpace(space, [...sequence], [], fillSpace),
-  )
-}
-*/
-
 /**
   @param {Sequence} sSrc a `sequence`
   @description Form a `subsequence` by unshifting `item`s from the given `sequence` until the given `space` is filled in the `subsequence`
@@ -199,64 +136,8 @@ function fillSchema(d, sSrc, s, fill) {
   return fill(d, sSrc, s, fill)
 }
 
-/*
-// this hasn't been run
-function formSideByOrderedSchema(schema, sSrc, s, formSide) {
-  if (sSrc.length === 0) return {s, schemaLeft: schema, reachedLimit: true}
-
-  const item = sSrc.shift()
-  if (!schema.includes(item.space)) return {
-    s, schemaLeft: schema, reachedLimit: true
-  }
-
-  schema.splice(schema.indexOf(item.space), 1)
-  s.push(item)
-
-  if (schema.length === 0) return {s, schemaLeft: schema, reachedLimit: false}
-  return formSide(schema, sSrc, s, formSide)
-}
-*/
-
-/**
-  @param {Space || Schema} spaceToFill
-  class Subsequence {
-    constructor(spaceToFill, items, location) {
-      this._spaceToFill = spaceToFill
-      this.items = items
-    }
-
-    get delta() {
-      return (Array.isArray(this._spaceToFill))
-        ? this._spaceToFill.reduce((d, i) => {
-          if (this.items.includes(i)) return d
-          d.push(i); return d
-        }, [])
-        : this._spaceToFill - this.size
-    }
-
-    set delta(x) {
-      throw new Error("delta is read-only")
-    }
-
-    get size() {
-      return this.items.reduce((sum, i) => sum+i, 0)
-    }
-
-    set size(x) {
-      throw new Error("size is read-only")
-    }
-
-    isDeltaEmpty() {
-      return Array.isArray(this.delta)
-        ? 0 === this.delta.length
-        : 0 === this.delta
-    }
-  }
-*/
-
 export {
   subsequencesSequences,
-  negateOverlaps, doNegateOverlaps, cascadeSubsequence,
   subsequences,
 
   fillSpace, fillSchema, fill,
